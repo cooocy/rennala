2ca48457450b29e31479ed955754b480dc8df4ee
cooocy <cooocy@foxmail.com>
2022-10-14 16:05:29 +0800

    Publish 0.0.1 using springboot2.x

--- src/main/java/ink/wulian/er/malenia/RequestLogAdvice.java
+++ src/main/java/ink/wulian/er/malenia/RequestLogAdvice.java
@@ -2,10 +2,12 @@ package ink.wulian.er.malenia;
 
 import cn.hutool.core.net.URLDecoder;
 import cn.hutool.core.util.IdUtil;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.OncePerRequestFilter;
