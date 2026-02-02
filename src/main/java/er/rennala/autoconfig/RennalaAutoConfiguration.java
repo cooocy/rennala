256cb664a232b2803e8303e6dfc28786ab6510b7
cooocy <cooocy@foxmail.com>
2026-02-02 16:52:47 +0800

    feature: Release 1.1.17.

--- src/main/java/er/rennala/autoconfig/RennalaAutoConfiguration.java
+++ src/main/java/er/rennala/autoconfig/RennalaAutoConfiguration.java
@@ -2,12 +2,13 @@ package er.rennala.autoconfig;
 
 import er.rennala.health.HealthController;
 import er.rennala.health.HealthProperties;
+import er.rennala.kits.SpringKits;
 import er.rennala.response.GlobalExceptionHandler;
 import er.rennala.z.AppProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-@Import({AppProperties.class, HealthController.class, HealthProperties.class, GlobalExceptionHandler.class})
+@Import({AppProperties.class, HealthController.class, HealthProperties.class, GlobalExceptionHandler.class, SpringKits.class})
 @AutoConfiguration
 public class RennalaAutoConfiguration {
 
